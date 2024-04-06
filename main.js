a728af2397426c7d3930248d5e3d4b01f74ad28e
Suiryuunin <suiryuu.nin@gmail.com>
2024-04-06 02:58:22 -0400

    reflection

--- main.js
+++ main.js
@@ -20,6 +20,13 @@ const material = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
 const ico = new THREE.Mesh( geometry, material );
 scene.add( ico );
 
+const geometry2 = new THREE.BoxGeometry( 2, 3, 2 );
+const material2 = new THREE.MeshStandardMaterial( { color: 0x00000 } );
+const ico2 = new THREE.Mesh( geometry2, material2 );
+ico2.name = "reflect";
+ico2.position.set(0, 0, 3);
+scene.add( ico2 );
+
 const planeG = new THREE.PlaneGeometry( 10, 10 );
 const gray = new THREE.MeshStandardMaterial({color: 0xAAAAAA, side: THREE.DoubleSide});
 const plane = new THREE.Mesh(planeG, gray);
@@ -45,11 +52,51 @@ let ps = Math.round(window.innerWidth/125/2)*2;
 
 const buffer = document.createElement("canvas").getContext("2d");
 
-renderer.render(scene, camera);
 
 let dir = new THREE.Vector3();
 
 const lightV = new THREE.Vector3(10, 10, 10);
+
+renderer.render(scene, camera);
+
+const calculateReflections = (i1, r) => {
+    dir.subVectors( r.ray.direction, i1[0].face.normal ).normalize();
+    dir.multiplyVectors(dir, new THREE.Vector3(1,1,-1))
+
+    const rc = new THREE.Raycaster(i1[0].point, dir);
+    const i2 = rc.intersectObjects(scene.children);
+    if (i2.length>0)
+    {
+        let c;
+        let theta;
+        if ( ico2 != i2[0].object )
+        {
+            c = i2[0].object.material.color;
+            dir.subVectors( lightV, i2[0].point ).normalize();
+            theta = i2[0].face.normal.angleTo(dir);
+        }
+        else if (i2.length > 1)
+        {
+            c = i2[1].object.material.color;
+            dir.subVectors( lightV, i2[1].point ).normalize();
+            theta = i2[1].face.normal.angleTo(dir);
+        }
+        else
+        {
+            c = i2[0].object.material.color;
+            dir.subVectors( lightV, i2[0].point ).normalize();
+            theta = i2[0].face.normal.angleTo(dir);
+        }
+
+        
+                            
+        let i = Math.cos(theta);
+
+        return {r: c.r*i, g: c.g*i, b: c.b*i};
+    }
+
+    return {r:0, g:0, b:0};
+};
 
 const updateRender = () =>
 {
@@ -65,21 +112,36 @@ const updateRender = () =>
             let intensity = 1;
             if (intersects.length > 0)
             {
-                dir.subVectors( lightV, intersects[0].point ).normalize();
-
-                const raycaster2 = new THREE.Raycaster(intersects[0].point, dir);
-                const intersects2 = raycaster2.intersectObjects(scene.children);
-                if (intersects2.length > 1 || (intersects2.length == 1 && intersects2[0].distance > 0.001))
+                if (intersects[0].object.name == "reflect")
                 {
-                    color = {r: 0, g: 0, b: 0};
+                    color = calculateReflections(intersects, raycaster);
                 }
                 else
                 {
-                    color = intersects[0].object.material.color;
-                    const theta = intersects[0].face.normal.angleTo(dir);
-                    
-                    intensity = Math.cos(theta);
+                    dir.subVectors( lightV, intersects[0].point ).normalize();
+
+                    const raycaster2 = new THREE.Raycaster(intersects[0].point, dir);
+                    const intersects2 = raycaster2.intersectObjects(scene.children);
+                    if (intersects2.length > 1 || (intersects2.length == 1 && intersects2[0].distance > 0.00001))
+                    {
+                        color = {r:0, g:0, b:0};
+                    }
+                    else
+                    {
+                        if (intersects[0].object.isMesh)
+                        {
+                            color = intersects[0].object.material.color;
+                            const theta = intersects[0].face.normal.angleTo(dir);
+                            
+                            intensity = Math.cos(theta);
+                        }
+                        else
+                        {
+                            intensity = 1;
+                        }
+                    }
                 }
+                
             }
             else
             {
